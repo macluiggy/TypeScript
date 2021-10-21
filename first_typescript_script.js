e8734adf6c71b82b81ca7dfd9abcd329986a7848
macluiggy <ferrinluiggy@gmail.com>
2021-10-21 14:48:18 -0500

    learning about types and interfaces

--- first_typescript_script.js
+++ first_typescript_script.js
@@ -1,1 +1,23 @@
 var hello = 'hello' + 3;
+/*const user: User = {
+    name: 'hayes',
+    id: 0,
+    age: 20
+}*/
+var UserAccount = /** @class */ (function () {
+    function UserAccount(name, id, age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+    return UserAccount;
+}());
+var user = new UserAccount('Morphy', 1, 1002);
+var MyBool = {};
+var house = {
+    WindowStates: 'broken'
+};
+function getLength(obj) {
+    return obj.length;
+}
+getLength(['jdjd', 'eue4']);
--- first_typescript_script.ts
+++ first_typescript_script.ts
@@ -1,2 +1,47 @@
 const hello:string = 'hello'+3
 
+interface User {
+	name: string;
+	id: number;
+	age: number;
+}
+
+/*const user: User = {
+	name: 'hayes',
+	id: 0,
+	age: 20
+}*/
+
+class UserAccount {
+	name: string;
+	id: number;
+	age: number
+
+	constructor(name: string, id: number, age: number) {
+		this.name = name;
+		this.id = id;
+		this.age = age
+	}
+}
+
+const user: User = new UserAccount('Morphy', 1, 1002)
+
+type MyBool = true | false
+
+const MyBool = {}
+
+type WindowStates = "open" | "closed" | "minimized";
+
+interface House {
+	WindowStates,
+}
+
+let house: House = {
+	WindowStates: 'broken'
+}
+
+function getLength(obj: string | string[] | number[] | object[]) {//number no puede ir debido a que length no se puede para ese typo
+	return obj.length
+}
+
+getLength(['jdjd', 'eue4'])
